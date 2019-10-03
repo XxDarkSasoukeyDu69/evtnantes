e918c710cd7e1c12406017c032d4c449b6e20123
XxDarkSasoukeyDu69 <32664521+XxDarkSasoukeyDu69@users.noreply.github.com>
2019-10-03 15:40:20 +0200

    Update router.js

--- src/router.js
+++ src/router.js
@@ -8,26 +8,6 @@ export default new Router({
     routes: [
         {
             path: '/',
-            name: 'home',
-            component: () => import('./components/home/Home.vue')
-        },
-        {
-            path: '/connexion',
-            name: 'connexion',
-            component: () => import('./components/authentification/connexion/Connexion.vue')
-        },
-        {
-            path: '/register',
-            name: 'register',
-            component: () => import('./components/authentification/register/Register.vue')
-        },
-        {
-            path: '/world',
-            name: 'world',
-            component: () => import('./components/world/World.vue')
-        },
-        {
-            path: '/cours',
             name: 'cours',
             component: () => import('./components/cours/cours')
         }
